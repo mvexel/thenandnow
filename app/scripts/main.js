e0398eaf1fabf4a4abb2601ce44e2e2163fcb034
Martijn van Exel <m@rtijn.org>
2015-11-30 22:24:00 -0700

    revert

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,7 @@
 	}).addTo(map);
 
 	/* add the 2007 overlay */
-	var overlay = L.tileLayer('http://24.11.75.220/osm/{z}/{x}/{y}.png').addTo(map);
+	var overlay = L.tileLayer('http://schaal.dyndns.org/osm/{z}/{x}/{y}.png').addTo(map);
 
 	var range = document.getElementById('range');
 
